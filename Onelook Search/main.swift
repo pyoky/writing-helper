f052afbd07e4aafdd201fd51808fb91c721a2cc2
Pyokyeong Son <pyoky3289@gmail.com>
2020-06-11 16:31:27 +0900

    Initial Commit

---
+++ Onelook Search/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Onelook Search
+//
+//  Created by Pyokyeong Son on 2020/06/11.
+//  Copyright © 2020 Pyokyeong Son. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
